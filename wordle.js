df1268c4457e3a2ca13364d743f9282ff0520ec7
1brahimmohamed <imimazim7@gmail.com>
2022-02-18 22:34:54 +0200

    added the verfication on word v 1.0

--- wordle.js
+++ wordle.js
@@ -29,13 +29,15 @@
 
     function keyListener(){
         document.addEventListener("keyup", (e)=>{
+
             if (gameOver)
                 return;
-            if ("KeyA" <= e.code && "KeyZ"){
+            if ("KeyA" <= e.code && "KeyZ" >= e.code){
                 if (col < width){
                     let curTitle = document.getElementById(row.toString() + '-' + col.toString());
                     if (curTitle.innerText === ""){
                         curTitle.innerText = e.code[3];
+                        curTitle.classList.add('filled');
                         col++;
                     }
                 }
@@ -44,12 +46,50 @@
                 if (0 < col && col <= width)
                     col--;
 
-                let curTitle = document.getElementById(row.toString() + '-' + col.toString())
+                let curTitle = document.getElementById(row.toString() + '-' + col.toString());
+                curTitle.classList.remove('filled');
                 curTitle.innerText = "";
             }
             else if (e.code === "Enter"){
+                updateBoard();
+                row++;
+                col=0;
+            }
 
+            if (!gameOver && row === height){
+                gameOver = true;
+                document.getElementById("answer").innerText = word;
             }
         })
     }
+
+    function updateBoard(){
+        let correct = 0;
+        let checkedletters;
+        for (let i = 0; i < width; i++){
+            let curTitle = document.getElementById(row.toString() + '-' + i.toString())
+            let letter = curTitle.innerText;
+            console.log(letter);
+            // correct position
+            if (word[i] === letter){
+                curTitle.classList.add('correct');
+                correct++;
+            }
+
+            // in the word?
+            else if (word.includes(letter)){
+                if (word[i] !== letter)
+                    curTitle.classList.add('present');
+            }
+            // not in the word
+            else
+                curTitle.classList.add('absent');
+
+            if (correct === width) {
+                gameOver = true;
+                document.getElementById("answer").innerText = "Congrats Lad";
+            }
+        }
+    }
+
 
