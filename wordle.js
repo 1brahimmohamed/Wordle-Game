00e0ecdeab83966cda1f33e4f8fd372a0704a656
1brahimmohamed <imimazim7@gmail.com>
2022-02-19 04:12:30 +0200

    finish most of the game

--- wordle.js
+++ wordle.js
@@ -1,10 +1,466 @@
 
-    let height = 6,     // number of attempts
+let words = ["WORDS",
+    "BEING",
+    "BELOW",
+    "BENCH",
+    "BILLY",
+    "BIRTH",
+    "BLACK",
+    "BLAME",
+    "BLIND",
+    "BLOCK",
+    "BLOOD",
+    "BOARD",
+    "BOOST",
+    "BOOTH",
+    "BOUND",
+    "BRAIN",
+    "BRAND",
+    "BREAD",
+    "BREAK",
+    "BREED",
+    "BRIEF",
+    "BRING",
+    "BROAD",
+    "BROKE",
+    "BROWN",
+    "BUILD",
+    "BUILT",
+    "BUYER",
+    "CABLE",
+    "CALIF",
+    "CARRY",
+    "CATCH",
+    "CAUSE",
+    "CHAIN",
+    "CHAIR",
+    "CHART",
+    "CHASE",
+    "CHEAP",
+    "CHECK",
+    "CHEST",
+    "CHIEF",
+    "CHILD",
+    "CHINA",
+    "CHOSE",
+    "CIVIL",
+    "CLAIM",
+    "CLASS",
+    "CLEAN",
+    "CLEAR",
+    "CLICK",
+    "CLOCK",
+    "CLOSE",
+    "COACH",
+    "COAST",
+    "COULD",
+    "COUNT",
+    "COURT",
+    "COVER",
+    "CRAFT",
+    "CRASH",
+    "CREAM",
+    "CRIME",
+    "CROSS",
+    "CROWD",
+    "CROWN",
+    "CURVE",
+    "CYCLE",
+    "DAILY",
+    "DANCE",
+    "DATED",
+    "DEALT",
+    "DEATH",
+    "DEBUT",
+    "DELAY",
+    "DEPTH",
+    "DOING",
+    "DOUBT",
+    "DOZEN",
+    "DRAFT",
+    "DRAMA",
+    "DRAWN",
+    "DREAM",
+    "DRESS",
+    "DRILL",
+    "DRINK",
+    "DRIVE",
+    "DROVE",
+    "DYING",
+    "EAGER",
+    "EARLY",
+    "EARTH",
+    "EIGHT",
+    "ELITE",
+    "EMPTY",
+    "ENEMY",
+    "ENJOY",
+    "ENTER",
+    "ENTRY",
+    "EQUAL",
+    "ERROR",
+    "EVENT",
+    "EVERY",
+    "EXACT",
+    "EXIST",
+    "EXTRA",
+    "FAITH",
+    "FAULT",
+    "FIBER",
+    "FIELD",
+    "FIFTH",
+    "FIFTY",
+    "FIGHT",
+    "FINAL",
+    "FIRST",
+    "FIXED",
+    "FLASH",
+    "FLEET",
+    "FLOOR",
+    "FLUID",
+    "FOCUS",
+    "FORCE",
+    "FORTH",
+    "FORTY",
+    "FORUM",
+    "FOUND",
+    "FRAME",
+    "FRANK",
+    "FRAUD",
+    "FRESH",
+    "FRONT",
+    "FRUIT",
+    "FULLY",
+    "FUNNY",
+    "GIANT",
+    "GIVEN",
+    "GLASS",
+    "GLOBE",
+    "GOING",
+    "GRACE",
+    "GRADE",
+    "GRAND",
+    "GRANT",
+    "GRASS",
+    "GREAT",
+    "GREEN",
+    "GROSS",
+    "GROUP",
+    "GROWN",
+    "GUARD",
+    "GUESS",
+    "GUEST",
+    "GUIDE",
+    "HAPPY",
+    "HARRY",
+    "HEART",
+    "HEAVY",
+    "HENCE",
+    "HENRY",
+    "HORSE",
+    "HOTEL",
+    "HOUSE",
+    "HUMAN",
+    "IDEAL",
+    "IMAGE",
+    "INDEX",
+    "INNER",
+    "INPUT",
+    "ISSUE",
+    "JAPAN",
+    "JIMMY",
+    "JOINT",
+    "JONES",
+    "JUDGE",
+    "KNOWN",
+    "LABEL",
+    "LARGE",
+    "LASER",
+    "LATER",
+    "LAUGH",
+    "LAYER",
+    "LEARN",
+    "LEASE",
+    "LEAST",
+    "LEAVE",
+    "LEGAL",
+    "LEVEL",
+    "LEWIS",
+    "LIGHT",
+    "LIMIT",
+    "LINKS",
+    "LIVES",
+    "LOCAL",
+    "LOGIC",
+    "LOOSE",
+    "LOWER",
+    "LUCKY",
+    "LUNCH",
+    "LYING",
+    "MAGIC",
+    "MAJOR",
+    "MAKER",
+    "MARCH",
+    "MARIA",
+    "MATCH",
+    "MAYBE",
+    "MAYOR",
+    "MEANT",
+    "MEDIA",
+    "METAL",
+    "MIGHT",
+    "MINOR",
+    "MINUS",
+    "MIXED",
+    "MODEL",
+    "MONEY",
+    "MONTH",
+    "MORAL",
+    "MOTOR",
+    "MOUNT",
+    "MOUSE",
+    "MOUTH",
+    "MOVIE",
+    "MUSIC",
+    "NEEDS",
+    "NEVER",
+    "NEWLY",
+    "NIGHT",
+    "NOISE",
+    "NORTH",
+    "NOTED",
+    "NOVEL",
+    "NURSE",
+    "OCCUR",
+    "OCEAN",
+    "OFFER",
+    "OFTEN",
+    "ORDER",
+    "OTHER",
+    "OUGHT",
+    "PAINT",
+    "PANEL",
+    "PAPER",
+    "PARTY",
+    "PEACE",
+    "PETER",
+    "PHASE",
+    "PHONE",
+    "PHOTO",
+    "PIECE",
+    "PILOT",
+    "PITCH",
+    "PLACE",
+    "PLAIN",
+    "PLANE",
+    "PLANT",
+    "PLATE",
+    "POINT",
+    "POUND",
+    "POWER",
+    "PRESS",
+    "PRICE",
+    "PRIDE",
+    "PRIME",
+    "PRINT",
+    "PRIOR",
+    "PRIZE",
+    "PROOF",
+    "PROUD",
+    "PROVE",
+    "QUEEN",
+    "QUICK",
+    "QUIET",
+    "QUITE",
+    "RADIO",
+    "RAISE",
+    "RANGE",
+    "RAPID",
+    "RATIO",
+    "REACH",
+    "READY",
+    "REFER",
+    "RIGHT",
+    "RIVAL",
+    "RIVER",
+    "ROBIN",
+    "ROGER",
+    "ROMAN",
+    "ROUGH",
+    "ROUND",
+    "ROUTE",
+    "ROYAL",
+    "RURAL",
+    "SCALE",
+    "SCENE",
+    "SCOPE",
+    "SCORE",
+    "SENSE",
+    "SERVE",
+    "SEVEN",
+    "SHALL",
+    "SHAPE",
+    "SHARE",
+    "SHARP",
+    "SHEET",
+    "SHELF",
+    "SHELL",
+    "SHIFT",
+    "SHIRT",
+    "SHOCK",
+    "SHOOT",
+    "SHORT",
+    "SHOWN",
+    "SIGHT",
+    "SINCE",
+    "SIXTH",
+    "SIXTY",
+    "SIZED",
+    "SKILL",
+    "SLEEP",
+    "SLIDE",
+    "SMALL",
+    "SMART",
+    "SMILE",
+    "SMITH",
+    "SMOKE",
+    "SOLID",
+    "SOLVE",
+    "SORRY",
+    "SOUND",
+    "SOUTH",
+    "SPACE",
+    "SPARE",
+    "SPEAK",
+    "SPEED",
+    "SPEND",
+    "SPENT",
+    "SPLIT",
+    "SPOKE",
+    "SPORT",
+    "STAFF",
+    "STAGE",
+    "STAKE",
+    "STAND",
+    "START",
+    "STATE",
+    "STEAM",
+    "STEEL",
+    "STICK",
+    "STILL",
+    "STOCK",
+    "STONE",
+    "STOOD",
+    "STORE",
+    "STORM",
+    "STORY",
+    "STRIP",
+    "STUCK",
+    "STUDY",
+    "STUFF",
+    "STYLE",
+    "SUGAR",
+    "SUITE",
+    "SUPER",
+    "SWEET",
+    "TABLE",
+    "TAKEN",
+    "TASTE",
+    "TAXES",
+    "TEACH",
+    "TEETH",
+    "TERRY",
+    "TEXAS",
+    "THANK",
+    "THEFT",
+    "THEIR",
+    "THEME",
+    "THERE",
+    "THESE",
+    "THICK",
+    "THING",
+    "THINK",
+    "THIRD",
+    "THOSE",
+    "THREE",
+    "THREW",
+    "THROW",
+    "TIGHT",
+    "TIMES",
+    "TIRED",
+    "TITLE",
+    "TODAY",
+    "TOPIC",
+    "TOTAL",
+    "TOUCH",
+    "TOUGH",
+    "TOWER",
+    "TRACK",
+    "TRADE",
+    "TRAIN",
+    "TREAT",
+    "TREND",
+    "TRIAL",
+    "TRIED",
+    "TRIES",
+    "TRUCK",
+    "TRULY",
+    "TRUST",
+    "TRUTH",
+    "TWICE",
+    "UNDER",
+    "UNDUE",
+    "UNION",
+    "UNITY",
+    "UNTIL",
+    "UPPER",
+    "UPSET",
+    "URBAN",
+    "USAGE",
+    "USUAL",
+    "VALID",
+    "VALUE",
+    "VIDEO",
+    "VIRUS",
+    "VISIT",
+    "VITAL",
+    "VOICE",
+    "WASTE",
+    "WATCH",
+    "WATER",
+    "WHEEL",
+    "WHERE",
+    "WHICH",
+    "WHILE",
+    "WHITE",
+    "WHOLE",
+    "WHOSE",
+    "WOMAN",
+    "WOMEN",
+    "WORLD",
+    "WORRY",
+    "WORSE",
+    "WORST",
+    "WORTH",
+    "WOULD",
+    "WOUND",
+    "WRITE",
+    "WRONG",
+    "WROTE",
+    "YIELD",
+    "YOUNG"
+]
+
+let height = 6,     // number of attempts
         width = 5,      // word length
         row = 0,    // current attempt
         col = 0,    // current letter for that attempt
-        gameOver = false,
-        word = "SQUID";
+        gameOver = false;
+
+        let wordIndex = Math.floor(Math.random() * (449 + 1));
+        console.log(wordIndex);
+        word = words[wordIndex];
+        console.log(word);
 
     window.onload = _ => {
         initialize();
@@ -12,29 +468,52 @@
 
     function initialize(){
         createGameBoard();
-        keyListener();
+        Listeners();
     }
 
     function createGameBoard(){
         for (let i = 0; i < height; i++){
             for (let j = 0; j < width; j++){
-                let title = document.createElement("span");
-                title.id = i.toString() + "-" + j.toString();
-                title.classList.add("title");
-                title.innerText = "";
-                document.getElementById("board").appendChild(title);
+                let flipCard = document.createElement("div");
+                let flipCardInner = document.createElement("div");
+                let flipCardFront = document.createElement("div");
+                let letterInside = document.createElement('span');
+
+                flipCard.classList.add('flip-card');
+                flipCardInner.classList.add('flip-card-inner');
+                flipCardFront.classList.add('flip-card-front');
+                letterInside.classList.add('title');
+                letterInside.id = i.toString() + "-" + j.toString() + "-front";
+                letterInside.innerText = "";
+                flipCardFront.appendChild(letterInside);
+                flipCardInner.appendChild(flipCardFront);
+                flipCard.appendChild(flipCardInner);
+                document.getElementById("board").appendChild(flipCard);
             }
         }
     }
 
-    function keyListener(){
+    function restGameBoard(){
+        for (let i = 0; i < height; i++){
+            for (let j = 0; j < width; j++){
+                let element = document.getElementById(row.toString() + '-' + i.toString() + '-front');
+                element.innerText = " ";
+                element.classList.remove('filled');
+            }
+        }
+        col = 0;
+        row = 0;
+        gameOver= false;
+    }
+
+    function Listeners(){
         document.addEventListener("keyup", (e)=>{
 
             if (gameOver)
                 return;
             if ("KeyA" <= e.code && "KeyZ" >= e.code){
                 if (col < width){
-                    let curTitle = document.getElementById(row.toString() + '-' + col.toString());
+                    let curTitle = document.getElementById(row.toString() + "-" + col.toString() + "-front");
                     if (curTitle.innerText === ""){
                         curTitle.innerText = e.code[3];
                         curTitle.classList.add('filled');
@@ -46,14 +525,12 @@
                 if (0 < col && col <= width)
                     col--;
 
-                let curTitle = document.getElementById(row.toString() + '-' + col.toString());
+                let curTitle = document.getElementById(row.toString() + "-" + col.toString()+ "-front");
                 curTitle.classList.remove('filled');
                 curTitle.innerText = "";
             }
             else if (e.code === "Enter"){
-                updateBoard();
-                row++;
-                col=0;
+                submitAnswer();
             }
 
             if (!gameOver && row === height){
@@ -61,15 +538,19 @@
                 document.getElementById("answer").innerText = word;
             }
         })
+        document.getElementById('enter').onclick = _ =>{
+            submitAnswer();
+        }
+        document.getElementById('replay').onclick = _ =>{
+            restGameBoard();
+        }
     }
 
     function updateBoard(){
         let correct = 0;
-        let checkedletters;
         for (let i = 0; i < width; i++){
-            let curTitle = document.getElementById(row.toString() + '-' + i.toString())
+            let curTitle = document.getElementById(row.toString() + '-' + i.toString() + '-front')
             let letter = curTitle.innerText;
-            console.log(letter);
             // correct position
             if (word[i] === letter){
                 curTitle.classList.add('correct');
@@ -84,12 +565,46 @@
             // not in the word
             else
                 curTitle.classList.add('absent');
+        }
+        if (correct === width) {
+            gameOver = true;
+            document.getElementById("answer").innerText = "Congrats Lad";
+        }
+    }
 
-            if (correct === width) {
-                gameOver = true;
-                document.getElementById("answer").innerText = "Congrats Lad";
+    function submitAnswer(){
+        let wordForCheck;
+        let noSpace = true;
+        for (let i = 0; i < width; i++) {
+            let curTitle = document.getElementById(row.toString() + '-' + i.toString() + '-front')
+            let letter = curTitle.innerText;
+            word[i] = letter;
+            if (letter === "")
+                noSpace= false;
+        }
+        if (noSpace){
+            if (wordValidator(wordForCheck)){
+                updateBoard();
+                row++;
+                col=0;
+            }
+            else {
+                document.getElementById('answer').innerText = "Please Enter a Valid Word";
             }
         }
+
+        else
+            document.getElementById('answer').innerText = "Please Don't Leave Spaces";
+    }
+
+    function wordValidator(checkedWord){
+        let found = false;
+        for (i = 0; i < words.length; i++){
+            if (checkedWord === words[i]){
+                found = true;
+            }
+        }
+        return found;
     }
 
 
